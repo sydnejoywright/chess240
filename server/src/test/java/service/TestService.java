2ddef4e7f0a638cacbc8ffd8f98d9a9d32cfc09f
Sydne Wright <sydnebrilliant@gmail.com>
2025-03-04 20:58:42 -0700

    did as much as i can tonight

--- server/src/test/java/service/TestService.java
+++ server/src/test/java/service/TestService.java
@@ -1,50 +1,107 @@
 package service;
+import model.GameData;
+import chess.ChessGame;
+import dataaccess.*;
+import exception.ResponseException;
+import model.*;
+import org.junit.jupiter.api.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.*;
 
 
 public class TestService {
+    private static UserService userService;
+    private static GameService gameService;
+    private static UserData firstUser;
+    private String firstUserAuth;
+
+    @BeforeAll
+    public static void init() throws DataAccessException {
+    }
+
+    @BeforeEach
+    public void setup() throws ResponseException, DataAccessException {
+    }
 
     @Test
-    public void testClearData(){}
+    public void testClearData(){
+    }
 
     @Test
-    public void testListGamesPositive(){}
+    public void testListGamesPositive(){
+        GameData gameData = new GameData();
+        gameData.setGameID(1);
+        gameData.setGameName("Hello hello");
+        List<GameData> list = new ArrayList<>();
+        list.add(gameData);
+        assertEquals(true, true);
+
+    }
 
     @Test
-    public void testListGamesNegative(){}
+    public void testListGamesNegative(){
+        assertEquals(true, true);
+    }
 
     @Test
-    public void testCreateGamePositive(){}
+    public void testCreateGamePositive(){
+        assertEquals(true, true);
+    }
 
     @Test
-    public void testCreateGameNegative(){}
+    public void testCreateGameNegative(){
+        assertEquals(true, true);
+    }
 
     @Test
-    public void testJoinGamePositive(){}
+    public void testJoinGamePositive(){
+        assertEquals(true, true);
+    }
 
     @Test
-    public void testJoinGameNegative(){}
+    public void testJoinGameNegative(){
+        assertEquals(true, true);
+    }
 
     //...............................................................
 
     @Test
-    public void testRegisterUserPositive(){}
+    public void testRegisterUserPositive(){
+        assertEquals(true, true);
+    }
 
     @Test
-    public void testRegisterUserNegative(){}
+    public void testRegisterUserNegative(){
+        assertEquals(true, true);
+    }
 
     @Test
-    public void testloginUserPositive(){}
+    public void testloginUserPositive(){
+        assertEquals(true, true);
+    }
 
     @Test
-    public void testLoginUserNegative(){}
+    public void testLoginUserNegative(){
+        assertEquals(true, true);
+    }
 
     @Test
-    public void testLogoutUserPositive(){}
+    public void testLogoutUserPositive(){
+        assertEquals(true, true);
+    }
 
     @Test
-    public void testLogoutUserNegative(){}
+    public void testLogoutUserNegative(){
+        assertEquals(true, true);
+    }
 
 
 
