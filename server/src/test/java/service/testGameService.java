5fac8899c40b4318704ac0ccdd8ecfd7cb902485
Sydne Wright <sydnebrilliant@gmail.com>
2025-03-04 20:35:02 -0700

    started writing tests

---
+++ server/src/test/java/service/testGameService.java
@@ -0,0 +1,16 @@
+package service;
+
+import chess.ChessGame;
+import org.junit.jupiter.api.*;
+import passoff.model.*;
+import passoff.server.TestServerFacade;
+
+import java.net.HttpURLConnection;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Locale;
+
+
+
+public class testGameService {
+}
---
+++ server/src/test/java/service/testUserService.java
@@ -0,0 +1,74 @@
+package service;
+
+import chess.ChessGame;
+import org.junit.jupiter.api.*;
+import passoff.model.*;
+import passoff.server.TestServerFacade;
+
+import java.net.HttpURLConnection;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Locale;
+
+
+public class testUserService {
+
+    @Test
+    public void testClearData(){}
+
+    @Test
+    public void testListGamesPositive(){}
+
+    @Test
+    public void testListGamesNegative(){}
+
+    @Test
+    public void testCreateGamePositive(){}
+
+    @Test
+    public void testCreateGameNegative(){}
+
+    @Test
+    public void testJoinGamePositive(){}
+
+    @Test
+    public void testJoinGameNegative(){}
+
+    //...............................................................
+
+    @Test
+    public void testRegisterUserPositive(){}
+
+    @Test
+    public void testRegisterUserNegative(){}
+
+    @Test
+    public void testloginUserPositive(){}
+
+    @Test
+    public void testLoginUserNegative(){}
+
+    @Test
+    public void testLogoutUserPositive(){}
+
+    @Test
+    public void testLogoutUserNegative(){}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
