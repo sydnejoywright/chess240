0b7aa80bab28f5e3daa0d2e19f6979a0ed1ee2d9
Sydne Wright <sydnebrilliant@gmail.com>
2025-03-23 20:35:29 -0600

    implemented playGame, still not sure how to keep track of gameID's from previous listing

--- client/src/main/java/ui/LoggedIn.java
+++ client/src/main/java/ui/LoggedIn.java
@@ -27,12 +27,12 @@ public class LoggedIn {
             var cmd = (tokens.length > 0) ? tokens[0] : "help";
             var params = Arrays.copyOfRange(tokens, 1, tokens.length);
             return switch (cmd) {
-                case "quit" -> "quit";
+                case "create" -> createGame(params);
+                case "list" -> listGames();
+                case "join" -> playGame(params);
+                case "observe" -> observeGame(params);
                 case "logout" -> logout();
-                case "create game" -> createGame(params);
-                case "list games" -> listGames();
-                case "play game" -> playGame(params);
-                case "observe game" -> observeGame(params);
+                case "quit" -> "quit";
                 default -> help();
             };
         } catch (ResponseException ex) {
@@ -40,46 +40,27 @@ public class LoggedIn {
         }
     }
 
-    public String logout() throws ResponseException {
-        try {
-            assertSignedIn();
-            server.logout();
-            return EscapeSequences.GREEN + "Successfully logged out" + EscapeSequences.RESET_TEXT_COLOR;
-
-        }catch(ResponseException e){
-            if(e.getMessage().equals("Error: unauthorized")){
-                return EscapeSequences.RED + "Logout unauthorized" + EscapeSequences.RESET_TEXT_COLOR;
-            }
-            else if(e.getMessage().equals("User is not logged in")){
-                return EscapeSequences.RED + "You must log in in order to perform this action" + EscapeSequences.RESET_TEXT_COLOR;
-            }
-            else{
-                return EscapeSequences.RED + "Cannot log out due to internal server error" + EscapeSequences.RESET_TEXT_COLOR;
-            }
-        }
-    }
-
     public String createGame(String... params) throws ResponseException {
-        try {
-            assertSignedIn();
-            server.createGame(params[0]);
-            System.out.println(EscapeSequences.GREEN + "Successfully created game" + EscapeSequences.RESET_TEXT_COLOR);
-            return listGames();
+        if(params.length == 2) {
+            try {
+                assertSignedIn();
+                server.createGame(params[0]);
+                System.out.println(EscapeSequences.GREEN + "Successfully created game" + EscapeSequences.RESET_TEXT_COLOR);
+                return listGames();
 
-        }catch(ResponseException e){
-            if(e.getMessage().equals("User is not logged in")){
-                return EscapeSequences.RED + "You must log in in order to perform this action" + EscapeSequences.RESET_TEXT_COLOR;
-            }
-            else if(e.getMessage().equals("Error: bad request")){
-                return EscapeSequences.RED + "Bad request" + EscapeSequences.RESET_TEXT_COLOR;
-            }
-            else if(e.getMessage().equals("Error: unauthorized")){
-                return EscapeSequences.RED + "This action is not authorized" + EscapeSequences.RESET_TEXT_COLOR;
-            }
-            else{
-                return EscapeSequences.RED + "Cannot log out due to internal server error" + EscapeSequences.RESET_TEXT_COLOR;
+            } catch (ResponseException e) {
+                if (e.getMessage().equals("User is not logged in")) {
+                    return EscapeSequences.RED + "You must log in in order to perform this action" + EscapeSequences.RESET_TEXT_COLOR;
+                } else if (e.getMessage().equals("Error: bad request")) {
+                    return EscapeSequences.RED + "Bad request" + EscapeSequences.RESET_TEXT_COLOR;
+                } else if (e.getMessage().equals("Error: unauthorized")) {
+                    return EscapeSequences.RED + "This action is not authorized" + EscapeSequences.RESET_TEXT_COLOR;
+                } else {
+                    return EscapeSequences.RED + "Cannot log out due to internal server error" + EscapeSequences.RESET_TEXT_COLOR;
+                }
             }
         }
+        return EscapeSequences.RED + "Expected <gameID> [WHITE][BLACK]" + EscapeSequences.RESET_TEXT_COLOR;
     }
 
     public String listGames() throws ResponseException {
@@ -99,11 +80,13 @@ public class LoggedIn {
         }
     }
 
+
     public String playGame(String teamColor, Integer gameID) throws ResponseException {
         try {
             assertSignedIn();
+            //SOMEHOW MY CLIENT NEEDS TO KEEP TRACK OF THE NUMBER OF THE GAMES FROM THE LAST TIME IT LISTED THE GAMES
             server.joinGame(gameID, teamColor);
-            return EscapeSequences.GREEN + "Successfully joined game" + EscapeSequences.RESET_TEXT_COLOR);
+            return EscapeSequences.GREEN + "Successfully joined game" + EscapeSequences.RESET_TEXT_COLOR;
 
         }catch (ResponseException e){
             if(e.getMessage().equals("User is not logged in")){
@@ -129,12 +112,30 @@ public class LoggedIn {
         return result.toString();
     }
 
+    public String logout() throws ResponseException {
+        try {
+            assertSignedIn();
+            server.logout();
+            return EscapeSequences.GREEN + "Successfully logged out" + EscapeSequences.RESET_TEXT_COLOR;
+
+        }catch(ResponseException e){
+            if(e.getMessage().equals("Error: unauthorized")){
+                return EscapeSequences.RED + "Logout unauthorized" + EscapeSequences.RESET_TEXT_COLOR;
+            }
+            else if(e.getMessage().equals("User is not logged in")){
+                return EscapeSequences.RED + "You must log in in order to perform this action" + EscapeSequences.RESET_TEXT_COLOR;
+            }
+            else{
+                return EscapeSequences.RED + "Cannot log out due to internal server error" + EscapeSequences.RESET_TEXT_COLOR;
+            }
+        }
+    }
 
     public String help() {
         return """
                 - create <NAME> - a game
                 - list - games
-                - join <ID> - a game
+                - join <ID> [WHITE][BLACK] - a game
                 - observe <ID> - a game
                 - logout - when you are done
                 - quit - playing chess
@@ -142,7 +143,7 @@ public class LoggedIn {
                 """;
     }
 
-    private String assertSignedIn() throws ResponseException{
+    private void assertSignedIn() throws ResponseException{
         if (state == State.LOGGEDOUT) {
             throw new ResponseException("User is not logged in");
         }
