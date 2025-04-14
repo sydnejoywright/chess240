5b48ce6f585ac0ad6ba3547427194f9b73f5a58f
zach-the <zachzbennett@gmail.com>
2025-04-14 01:28:02 -0600

    make move

--- shared/src/main/java/websocket/commands/MakeMoveCommand.java
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -1,7 +1,9 @@
 package websocket.commands;
 
+import chess.ChessGame;
 import chess.ChessMove;
 import chess.ChessPosition;
+import model.GameData;
 
-public record MakeMoveCommand(UserGameCommand.CommandType commandType, ChessMove move, int gameID, String authToken) {
+public record MakeMoveCommand(UserGameCommand.CommandType commandType, ChessMove move, int gameID, String authToken, GameData currentGameData) {
 }
