dfb9266d3a155c581e77d99aed8e8aa733a5193b
zach-the <zachzbennett@gmail.com>
2025-04-14 01:25:34 -0600

    gameplayuii

--- client/src/main/java/ui/GamePlayUI.java
+++ client/src/main/java/ui/GamePlayUI.java
@@ -1,23 +1,19 @@
 package ui;
 
-import chess.ChessBoard;
-import chess.ChessGame;
-import chess.ChessMove;
-import chess.ChessPosition;
+import chess.*;
 import exception.ResponseException;
+import model.AuthtokenData;
 import model.GameData;
-import ui.serverfacade.ServerFacade;
 import websocket.WebSocketClient;
 //import websocket.WebSocketFacade;
-import websocket.commands.ConnectCommand;
+import websocket.commands.LeaveCommand;
+import websocket.commands.MakeMoveCommand;
 import websocket.commands.UserGameCommand;
 import com.google.gson.Gson;
 
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 
 import static ui.EscapeSequences.GREEN;
@@ -27,25 +23,30 @@ public class GamePlayUI {
     private String authToken;
     private String username;
     private int gameID;
+    private String gameName;
 //    private WebSocketFacade client;
     private HashMap<Integer, Integer> gameRefs;
     private boolean isPlayer;
     private WebSocketClient client;
     private ChessGame.TeamColor asTeam;
     private String serverUrl;
+    private static final List<String> LETTERS = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h");
 
     public GamePlayUI(String username, String authToken, GameData gameData, ChessGame.TeamColor asTeam, Boolean isPlayer, int gameID) throws ResponseException, IOException {
         this.authToken = authToken;
         this.username = username;
         this.asTeam = asTeam;
+        this.gameName = gameData.getGameName();
         this.serverUrl = "http://localhost:8080";
-        this.client = new WebSocketClient(serverUrl, gameData.getChessGame(), asTeam);
+        this.client = new WebSocketClient(serverUrl, asTeam, gameData, username, this.gameName);
         this.gameID = gameID;
         client.sendMessage(new Gson().toJson(new UserGameCommand(UserGameCommand.CommandType.CONNECT, authToken, gameID)));
+        this.isPlayer = isPlayer;
     }
 
     public String run() {
         System.out.print(help());
+        try{redrawBoard();} catch (Exception e) {}
         Scanner scanner = new Scanner(System.in);
         var result = "";
         while (!result.equals("quit")) {
@@ -63,10 +64,17 @@ public class GamePlayUI {
         System.out.println();
         return result;
     }
-
-    private void printPrompt() {
-        System.out.print("\n" + EscapeSequences.RESET_TEXT_COLOR + "[LOGGED IN] " + username +" >>> " + GREEN);
+    private void removePrompt() {
+        System.out.print("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");
+        for (int i = 0; i < username.length() + gameName.length() + asTeam.toString().length(); i++) {
+            System.out.print("\b");
+        }
     }
+    private void printPrompt() {
+        removePrompt();
+        System.out.print(EscapeSequences.RESET_TEXT_COLOR + EscapeSequences.BLUE + "[" + username + ": playing in " + gameName + " as " + asTeam +"] >>> " + GREEN);
+    }
+
 
     public String eval(String input) {
         try {
@@ -83,24 +91,25 @@ public class GamePlayUI {
                 default -> help();
             };
         } catch (ResponseException ex) {
-            return ex.getMessage();
+            System.out.println("Problem: " + ex.getMessage());
         } catch (IOException e) {
+            return e.getMessage();
+        } catch (InvalidMoveException e) {
             throw new RuntimeException(e);
         }
+        return "";
     }
 
 
     public String redrawBoard () throws ResponseException{
+        removePrompt();
         client.redraw(asTeam);
         return "";
     };
 
     public String leaveGame () throws ResponseException, IOException {
-        System.out.println("beginning");
-
         try {
-            client.sendMessage(new Gson().toJson(new UserGameCommand(UserGameCommand.CommandType.LEAVE, authToken, gameID)));
-            System.out.println("yoo");
+            client.sendMessage(new Gson().toJson(new LeaveCommand(UserGameCommand.CommandType.LEAVE, new AuthtokenData(username, authToken), gameID, asTeam)));
             return "Leaving Game...\n" + new LoggedIn(serverUrl, authToken, username).run();
         } catch (Exception e) {
             System.out.println("Exception caught in sendMessage websocketClient: " + e.getMessage());
@@ -108,12 +117,102 @@ public class GamePlayUI {
         return "";
     };
 
+    private void executeMove(GameData currentData, ChessGame currentGame, ChessMove move) throws InvalidMoveException {
+        currentGame.makeMove(move);
+        currentData.setChessGame(currentGame);
+        try {
+            client.sendMessage(new Gson().toJson(new MakeMoveCommand(UserGameCommand.CommandType.MAKE_MOVE, move, gameID, authToken, currentData)));
+        } catch (IOException e) {
+            System.out.println("Exception caught in sendMessage websocketClient: " + e.getMessage() + "request came from makeMove gameplay");
+        }
+    }
 
-    public String makeMove(String... params) throws ResponseException{
+    public String makeMove(String... params) throws ResponseException, IOException, InvalidMoveException {
+        GameData currentGameData = client.getCurrentGameData();
+        ChessGame currentGame = currentGameData.getChessGame();
+//        System.out.println("It's this person's turn: " + currentGame.getTeamTurn().toString());
+
+        if(currentGame.getTeamTurn()!=asTeam){
+            return EscapeSequences.RED + "It's not your turn\n" + EscapeSequences.RESET_TEXT_COLOR;
+        }
         if(params.length == 2) {
+            //did the user give me two valid moves ie letter followed by number?
+            String col1 = String.valueOf(params[0].charAt(0)).toLowerCase();
+            String col2 = String.valueOf(params[1].charAt(0)).toLowerCase();
+
+            if (!LETTERS.contains(col1) || !Character.isDigit(params[0].charAt(1))) {
+                return EscapeSequences.RED + "Both of your parameters need to be in the form [letter <a-h>][number <1-8>]\n" + EscapeSequences.RESET_TEXT_COLOR;
+            } else if (!LETTERS.contains(col2) || !Character.isDigit(params[1].charAt(1))) {
+                return EscapeSequences.RED + "Both of your parameters need to be in the form [letter <a-h>][number <1-8>]\n" + EscapeSequences.RESET_TEXT_COLOR;
+            }
+            //create a chess move, which requires changing letters to numbers.
+            int from = convert_letters(col1);
+            int to = convert_letters(col2);
+
+            ChessPosition start = new ChessPosition(Integer.parseInt(String.valueOf(params[0].charAt(1))), from);
+            ChessPosition end = new ChessPosition(Integer.parseInt(String.valueOf(params[1].charAt(1))), to);
+            ChessMove move = new ChessMove(start, end, null);
+
+            ChessPiece piece = currentGame.getBoard().getPiece(start);
+            //if they try to move a piece that isn't there
+            if(piece == null){
+                return EscapeSequences.RED + "There is no piece at that position, check your parameters and try again\n" + EscapeSequences.RESET_TEXT_COLOR;
+            }
+            //check if the piece they are trying to move is their own
+            if(!(piece.getTeamColor() == asTeam)){
+                return EscapeSequences.RED + "That piece doesn't belong to you\n" + EscapeSequences.RESET_TEXT_COLOR;
+            }
+            else {
+                // check if it's a pawn, and if so it will need promotion
+                if (piece.getPieceType().equals(ChessPiece.PieceType.PAWN)){
+                    if ((asTeam == ChessGame.TeamColor.WHITE && start.getRow() == 7) || (asTeam == ChessGame.TeamColor.BLACK && start.getRow() == 2)) {
+                        boolean selectedPiece = false;
+                        while (!selectedPiece) {
+                            System.out.print(EscapeSequences.SET_TEXT_COLOR_MAGENTA + "\tWhat piece do you want to promote to?\n\t" + EscapeSequences.RESET_TEXT_COLOR);
+                            Scanner scanner = new Scanner(System.in);
+                            String line = scanner.nextLine();
+                            var tokens = line.toLowerCase().split(" ");
+                            if (tokens.length > 1) {
+                                System.out.println(EscapeSequences.RED + "\tYour piece should be one word. Here are your options: \n\tQUEEN, KNIGHT, ROOK, BISHOP" + EscapeSequences.RESET_TEXT_COLOR);
+                            } else {
+                                switch (tokens[0]) {
+                                    case "bishop" ->
+                                    { executeMove(currentGameData, currentGame, new ChessMove(move.getStartPosition(), move.getEndPosition(), ChessPiece.PieceType.BISHOP)); selectedPiece = true; break; }
+                                    case "queen" ->
+                                    { executeMove(currentGameData, currentGame, new ChessMove(move.getStartPosition(), move.getEndPosition(), ChessPiece.PieceType.QUEEN)); selectedPiece = true; break; }
+                                    case "rook" ->
+                                    { executeMove(currentGameData, currentGame, new ChessMove(move.getStartPosition(), move.getEndPosition(), ChessPiece.PieceType.ROOK)); selectedPiece = true; break; }
+                                    case "knight" ->
+                                    { executeMove(currentGameData, currentGame, new ChessMove(move.getStartPosition(), move.getEndPosition(), ChessPiece.PieceType.KNIGHT)); selectedPiece = true; break; }
+                                    case null, default ->
+                                            System.out.println(EscapeSequences.RED + "\tThat piece isn't valid. Here are your options: \n\tQUEEN, KNIGHT, ROOK, BISHOP" + EscapeSequences.RESET_TEXT_COLOR);
+                                }
+                            }
+                        }
+                    }
+                    executeMove(currentGameData, currentGame, move);
+                }
+                // if it's not a pawn
+                else {
+                    Collection<ChessMove> moves = piece.pieceMoves(currentGame.getBoard(), start);
+                    boolean moveHappened = false;
+                    for (ChessMove currMove : piece.pieceMoves(currentGame.getBoard(), start)) {
+                        if (moveHappened) { continue; }
+                        if (currMove.equals(move)) {
+                            executeMove(currentGameData, currentGame, move);
+                            moveHappened = true;
+                        }
+                    }
+                    if (!moveHappened){
+//                        System.out.println(moves);
+//                        System.out.println(move);
+                        return EscapeSequences.RED + "That's not a valid move for that piece\n" + EscapeSequences.RESET_TEXT_COLOR;
+                    }
+                }
+            }
             return "";
         }
-        return EscapeSequences.RED + "Check your parameters: Expected <start position> <end position>" + EscapeSequences.RESET_TEXT_COLOR;
+        return EscapeSequences.RED + "Check your parameters: Expected <start position> <end position>\n" + EscapeSequences.RESET_TEXT_COLOR;
     };
     public String resignGame() throws ResponseException{
         return "";
@@ -124,14 +223,27 @@ public class GamePlayUI {
            return "";
 
        }
-        return EscapeSequences.RED + "Check your parameters: Expected <position>" + EscapeSequences.RESET_TEXT_COLOR;
+        return EscapeSequences.RED + "Check your parameters: Expected <position>\n" + EscapeSequences.RESET_TEXT_COLOR;
     };
 
 
+    public int convert_letters(String letter) {
+        return switch (letter) {
+            case "a" -> 1;
+            case "b" -> 2;
+            case "c" -> 3;
+            case "d" -> 4;
+            case "e" -> 5;
+            case "f" -> 6;
+            case "g" -> 7;
+            case "h" -> 8;
+            default -> -1;  // return -1 to signal an invalid input
+        };
+    }
 
     public String help() {
         if(isPlayer) {
-            return """
+            return EscapeSequences.SET_TEXT_COLOR_YELLOW +"""
                     - redraw - redraws board
                     - leave
                     - move
@@ -141,7 +253,7 @@ public class GamePlayUI {
                     - help - with possible commands
                     """;
         }else{
-            return """
+            return EscapeSequences.SET_TEXT_COLOR_YELLOW +"""
                     - redraw - redraws board
                     - leave
                     - highlight
